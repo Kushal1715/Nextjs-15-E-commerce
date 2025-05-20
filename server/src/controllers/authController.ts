1ff6e44a01178711d125977a43ea6bc31cad743e
Kushal1715 <kushalkhadka77@gmail.com>
2025-05-20 21:17:57 +0545

    generating and setting token while login

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -1,6 +1,39 @@
 import { Request, Response } from "express";
 import { prisma } from "../server";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { v4 as uuidv4 } from "uuid";
+
+const generateToken = (userId: string, email: string, role: string) => {
+  const accessToken = jwt.sign(
+    { userId, email, role },
+    process.env.JWT_SECRET!,
+    { expiresIn: "60m" }
+  );
+
+  const refreshToken = uuidv4();
+
+  return { accessToken, refreshToken };
+};
+
+const setToken = async (
+  res: Response,
+  accessToken: string,
+  refreshToken: string
+) => {
+  res.cookie("accessToken", accessToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: 60 * 60 * 1000,
+  });
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    maxAge: 7 * 24 * 60 * 60,
+  });
+};
 
 export const register = async (req: Request, res: Response): Promise<void> => {
   try {
@@ -27,5 +60,49 @@ export const register = async (req: Request, res: Response): Promise<void> => {
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "registration failed" });
+  }
+};
+
+export const login = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { email, password } = req.body;
+
+    const userAlreadyExists = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!userAlreadyExists) {
+      res.status(400).json({
+        success: false,
+        message: "invalid credentials",
+      });
+      return;
+    }
+
+    const isPasswordValid = await bcrypt.compare(
+      password,
+      userAlreadyExists.password
+    );
+
+    if (!isPasswordValid) {
+      res.status(400).json({
+        success: false,
+        message: "invalid credentials",
+      });
+      return;
+    }
+
+    const { accessToken, refreshToken } = generateToken(
+      userAlreadyExists.id,
+      userAlreadyExists.email,
+      userAlreadyExists.role
+    );
+
+    await setToken(res, accessToken, refreshToken);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "login failed",
+    });
   }
 };
