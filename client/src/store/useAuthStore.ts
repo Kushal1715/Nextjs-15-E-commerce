a9bbe4805a2e2d0d9bdc26b7417d3e9feb35cce1
Kushal1715 <kushalkhadka77@gmail.com>
2025-05-28 18:04:04 +0545

    making user and AuthStore type in useAuthStore.ts

---
+++ client/src/store/useAuthStore.ts
@@ -0,0 +1,20 @@
+type User = {
+  id: string;
+  name: string | null;
+  email: string;
+  role: "USER" | "SUPER_ADMIN";
+};
+
+type AuthStore = {
+  user: User | null;
+  isLoading: boolean;
+  error: string | null;
+  register: (
+    name: string,
+    email: string,
+    password: string
+  ) => Promise<string | null>;
+  login: (email: string, password: string) => Promise<boolean>;
+  logout: () => Promise<void>;
+  refreshAccessToken: () => Promise<boolean>;
+};
