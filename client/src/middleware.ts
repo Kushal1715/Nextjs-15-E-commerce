0b8eed1a114d0f3f7a58fbc07c3ab162f089012f
Kushal1715 <kushalkhadka77@gmail.com>
2025-05-31 23:56:49 +0545

    middleware completed

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -70,4 +70,14 @@ export async function middleware(request: NextRequest) {
       }
     }
   }
+
+  if (!publicRoutes.includes(pathname)) {
+    return NextResponse.redirect(new URL("/auth/login", request.url));
+  }
+
+  return NextResponse.next();
 }
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
