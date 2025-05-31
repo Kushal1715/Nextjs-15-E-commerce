31b4babf757ada3f81bde6379723c19300093204
Kushal1715 <kushalkhadka77@gmail.com>
2025-05-31 23:53:03 +0545

    redirecting routes in middleware

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -1,4 +1,5 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
+import { jwtVerify } from "jose";
 
 const publicRoutes = ["/auth/login", "/auth/register"];
 const superAdminRoutes = ["/super-admin", "/super-admin/:path*"];
@@ -9,5 +10,36 @@ export const middleware = async (request: NextRequest) => {
   const { pathname } = request.nextUrl;
 
   if (accessToken) {
+    try {
+      const { payload } = await jwtVerify(
+        accessToken,
+        new TextEncoder().encode(process.env.JWT_SECRET)
+      );
+
+      const { role } = payload as { role: string };
+
+      if (publicRoutes.includes(pathname)) {
+        return NextResponse.redirect(
+          new URL(
+            role === "SUPER_ADMIN" ? "/super-admin" : "/home",
+            request.url
+          )
+        );
+      }
+
+      if (
+        role === "SUPER_ADMIN" &&
+        userRoutes.some((route) => pathname.startsWith(route))
+      ) {
+        return NextResponse.redirect(new URL("/super-admin", request.url));
+      }
+
+      if (
+        role !== "SUPER_ADMIN" &&
+        superAdminRoutes.some((route) => pathname.startsWith(route))
+      ) {
+        return NextResponse.redirect(new URL("/home", request.url));
+      }
+    } catch (error) {}
   }
 };
