aefe0d559bc5d32192d016ca30dfa28a8b59d5ad
Kushal1715 <kushalkhadka77@gmail.com>
2025-05-31 23:45:45 +0545

    getting accessToken and current pathname

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -1,3 +1,13 @@
+import { NextRequest } from "next/server";
+
 const publicRoutes = ["/auth/login", "/auth/register"];
 const superAdminRoutes = ["/super-admin", "/super-admin/:path*"];
 const userRoutes = ["/home"];
+
+export const middleware = async (request: NextRequest) => {
+  const accessToken = request.cookies.get("accessToken")?.value;
+  const { pathname } = request.nextUrl;
+
+  if (accessToken) {
+  }
+};
