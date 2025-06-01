44ef43a06a21ee9fe6c7efaa05928b5a6e10a95a
Kushal1715 <kushalkhadka77@gmail.com>
2025-06-01 19:18:04 +0545

    superadmin sidebar completed

--- client/src/app/super-admin/layout.tsx
+++ client/src/app/super-admin/layout.tsx
@@ -1,9 +1,18 @@
 "use client";
+import SuperAdminSidebar from "@/components/super-admin/sidebar";
 import React, { useState } from "react";
 
 const SuperAdminLayout = ({ children }: { children: React.ReactNode }) => {
-  const [openSidebar, setOpenSidebar] = useState(false);
-  return <div>SuperAdminLayout</div>;
+  const [openSidebar, setOpenSidebar] = useState(true);
+  return (
+    <div className="min-h-screen bg-background">
+      <SuperAdminSidebar
+        isOpen={openSidebar}
+        toggle={() => setOpenSidebar(!openSidebar)}
+      />
+      <div>{children}</div>
+    </div>
+  );
 };
 
 export default SuperAdminLayout;
--- client/src/components/super-admin/sidebar.tsx
+++ client/src/components/super-admin/sidebar.tsx
@@ -1,13 +1,109 @@
 "use client";
+import { cn } from "@/lib/utils";
 import React from "react";
+import { Button } from "../ui/button";
+import {
+  ChevronLeft,
+  ChevronRight,
+  FileText,
+  ListOrdered,
+  LogOut,
+  Package,
+  Printer,
+  SendToBack,
+  Settings,
+} from "lucide-react";
+import { useRouter } from "next/navigation";
 
 interface SidebarProps {
   isOpen: boolean;
   toggle: () => void;
 }
 
+const menuItems = [
+  {
+    name: "Products",
+    icon: Package,
+    href: "/super-admin/products/list",
+  },
+  {
+    name: "Add New Product",
+    icon: Printer,
+    href: "/super-admin/products/add",
+  },
+  {
+    name: "Orders",
+    icon: SendToBack,
+    href: "/super-admin/orders",
+  },
+  {
+    name: "Coupons",
+    icon: FileText,
+    href: "/super-admin/coupons/list",
+  },
+  {
+    name: "Create Coupon",
+    icon: ListOrdered,
+    href: "/super-admin/coupons/add",
+  },
+  {
+    name: "Settings",
+    icon: Settings,
+    href: "/super-admin/settings",
+  },
+  {
+    name: "logout",
+    icon: LogOut,
+    href: "",
+  },
+];
+
 const SuperAdminSidebar = ({ isOpen, toggle }: SidebarProps) => {
-  return <div>SuperAdminSidebar</div>;
+  const router = useRouter();
+
+  const handleLogout = async () => {};
+  return (
+    <div
+      className={cn(
+        "min-h-screen bg-background fixed top-0 left-0 transition-all duration-300 border-r-2",
+        isOpen ? "w-64" : "w-16"
+      )}
+    >
+      <div className="flex h-16 items-center justify-between px-6">
+        <h1 className={cn(!isOpen && "hidden", "font-bold text-xl")}>
+          Admin Panel
+        </h1>
+        <Button
+          variant={"ghost"}
+          size={"icon"}
+          className="ml-auto"
+          onClick={toggle}
+        >
+          {isOpen ? (
+            <ChevronLeft className="h-4 w-4" />
+          ) : (
+            <ChevronRight className="h-4 w-4" />
+          )}
+        </Button>
+      </div>
+      <div className="mt-4 space-y-1 py-4">
+        {menuItems.map((item) => (
+          <div
+            onClick={
+              item.name === "logout"
+                ? handleLogout
+                : () => router.push(item.href)
+            }
+            key={item.name}
+            className="flex items-center px-6 py-2 cursor-pointer hover:bg-gray-200 transition-colors gap-4"
+          >
+            <item.icon className="h-4 w-4" />
+            {isOpen && item.name}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default SuperAdminSidebar;
