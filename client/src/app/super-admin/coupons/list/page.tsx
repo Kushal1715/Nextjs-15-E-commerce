eace92ba3663410a25d8d3b461d19e85acfc6374
Kushal1715 <kushalkhadka77@gmail.com>
2025-06-04 13:58:45 +0545

    creating all pages for super admin

---
+++ client/src/app/super-admin/coupons/list/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const SuperAdminCouponsListingPage = () => {
+  return <div>SuperAdminCouponsListingPage</div>;
+};
+
+export default SuperAdminCouponsListingPage;
--- client/src/app/super-admin/page.tsx
+++ client/src/app/super-admin/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const SuperAdmin = () => {
-  return <div>SuperAdmin</div>;
+  return <div> super admin product listing page</div>;
 };
 
 export default SuperAdmin;
