1c6615ab725b15d645ff9d48b14e6777794b5549
Kushal1715 <kushalkhadka77@gmail.com>
2025-06-05 23:51:59 +0545

    making middlware to authenticate jwt

--- server/src/controllers/productController.ts
+++ server/src/controllers/productController.ts
@@ -8,3 +8,39 @@ export interface AuthenticatedRequest extends Request {
     role: string;
   };
 }
+
+export const authenticateJwt = (
+  req: AuthenticatedRequest,
+  res: Response,
+  next: NextFunction
+) => {
+  const accessToken = req.cookies.accessToken;
+  if (!accessToken) {
+    res
+      .status(401)
+      .json({ success: false, error: "Access token is not present" });
+    return;
+  }
+
+  jwtVerify(accessToken, new TextEncoder().encode(process.env.JWT_SECRET))
+    .then((res) => {
+      const payload = res.payload as JWTPayload & {
+        userId: string;
+        email: string;
+        role: string;
+      };
+
+      req.user = {
+        userId: payload.userId,
+        email: payload.email,
+        role: payload.role,
+      };
+      next();
+    })
+    .catch((e) => {
+      console.error(e);
+      res
+        .status(401)
+        .json({ success: false, error: "Access token is not present" });
+    });
+};
