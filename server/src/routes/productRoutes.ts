afe8195524d01beb0c06cadafa576c8245614f4e
Kushal1715 <kushalkhadka77@gmail.com>
2025-06-10 00:34:49 +0545

    making product routes

--- server/src/routes/productRoutes.ts
+++ server/src/routes/productRoutes.ts
@@ -1,7 +1,34 @@
 import express from "express";
+import { authenticateJwt, isSuperAdmin } from "../middleware/authMiddleware";
+import { upload } from "../middleware/uploadMiddleware";
+import {
+  createProduct,
+  deleteProductByAdmin,
+  fetchAllProductsForAdmin,
+  fetchProductById,
+  updateProductByAdmin,
+} from "../controllers/productController";
 
 const router = express.Router();
 
-router.post("/create");
+router.post(
+  "/create-new-product",
+  authenticateJwt,
+  isSuperAdmin,
+  upload.array("images", 5),
+  createProduct
+);
+router.get(
+  "/fetch-admin-products",
+  authenticateJwt,
+  isSuperAdmin,
+  fetchAllProductsForAdmin
+);
+
+router.get("/:id", authenticateJwt, fetchProductById);
+
+router.patch("/:id", authenticateJwt, isSuperAdmin, updateProductByAdmin);
+
+router.delete("/:id", authenticateJwt, isSuperAdmin, deleteProductByAdmin);
 
 export default router;
