e4ea66e0f216b06065bdd6b9ab6ee3846aa1a44c
Kushal1715 <kushalkhadka77@gmail.com>
2025-06-16 23:46:24 +0545

    fetching products in listing page

--- client/src/app/super-admin/products/list/page.tsx
+++ client/src/app/super-admin/products/list/page.tsx
@@ -1,6 +1,14 @@
-import React from "react";
+"use client";
+import { useProductStore } from "@/store/productStore";
+import React, { useEffect } from "react";
 
 const SuperAdminProductListingPage = () => {
+  const { products, fetchAllProductsForAdmin } = useProductStore();
+
+  useEffect(() => {
+    fetchAllProductsForAdmin();
+  }, []);
+  console.log(products);
   return <div>SuperAdminProductListingPage</div>;
 };
 
