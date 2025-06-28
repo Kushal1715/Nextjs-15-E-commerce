faf33bdb8ebaae20e530a28dead0d0cfb888d641
Kushal1715 <kushalkhadka77@gmail.com>
2025-06-28 22:52:39 +0545

    fetching feature banners in home page

--- client/src/app/home/page.tsx
+++ client/src/app/home/page.tsx
@@ -1,6 +1,20 @@
-import React from "react";
+"use client";
+import { useSettingsStore } from "@/store/useSettingStore";
+import React, { useEffect } from "react";
 
 const Home = () => {
+  const {
+    featureBanners,
+    featuredProducts,
+    fetchFeatureBanners,
+    fetchFeaturedProducts,
+  } = useSettingsStore();
+
+  useEffect(() => {
+    fetchFeatureBanners();
+  }, [fetchFeatureBanners, fetchFeaturedProducts]);
+
+  console.log(featureBanners);
   return <div>Home</div>;
 };
 
