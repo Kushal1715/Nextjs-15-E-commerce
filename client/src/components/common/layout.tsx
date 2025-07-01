2e9bf122095914a4bd514599546d41475e7c4358
Kushal1715 <kushalkhadka77@gmail.com>
2025-07-01 21:38:56 +0545

    hiding header in auth/super admin pages

--- client/src/components/common/layout.tsx
+++ client/src/components/common/layout.tsx
@@ -1,12 +1,21 @@
+"use client";
 import React, { ReactNode } from "react";
 import Header from "../client/header";
+import { usePathname } from "next/navigation";
 
 const CommonLayout = ({ children }: { children: ReactNode }) => {
+  const pathName = usePathname();
+
+  const showHeader =
+    pathName.startsWith("/auth") || pathName.startsWith("/super-admin");
+
   return (
     <div>
-      <div>
-        <Header />
-      </div>
+      {!showHeader && (
+        <div>
+          <Header />
+        </div>
+      )}
       <main>{children}</main>
     </div>
   );
