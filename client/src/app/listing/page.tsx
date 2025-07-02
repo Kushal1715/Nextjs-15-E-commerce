d1e9eba79f3886b1dd4008b55bb93b435a1ae92f
Kushal1715 <kushalkhadka77@gmail.com>
2025-07-02 15:06:09 +0545

    creating banner for listing page

--- client/src/app/listing/page.tsx
+++ client/src/app/listing/page.tsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 const ProductListingPage = () => {
-  return <div>ProductListingPage</div>;
+  return (
+    <div className="min-h-screen">
+      <div className="relative h-[300px]">
+        <img
+          src="https://images.unsplash.com/photo-1469334031218-e382a71b716b?q=80&w=2070&auto=format&fit=crop"
+          alt="listing page banner image"
+          className="absolute w-full h-full object-cover"
+        />
+        <div className="absolute inset-0 flex items-center justify-center">
+          <div className="text-center text-white">
+            <h1 className="font-bold text-4xl">HOT COLLECTION</h1>
+            <p className="text-lg">Discover our latest collection</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductListingPage;
