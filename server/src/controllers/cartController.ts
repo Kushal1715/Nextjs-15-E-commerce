1c930676742bf20c9af22b7d40254c8a6ff21564
Kushal1715 <kushalkhadka77@gmail.com>
2025-07-10 20:26:49 +0545

    addtocart controller completed

---
+++ server/src/controllers/cartController.ts
@@ -0,0 +1,70 @@
+import { Response } from "express";
+import { AuthenticatedRequest } from "../middleware/authMiddleware";
+import { prisma } from "../server";
+
+export const AddToCart = async (
+  req: AuthenticatedRequest,
+  res: Response
+): Promise<void> => {
+  try {
+    const userId = req.user?.userId;
+    const { productId, quantity, size, color } = req.body;
+
+    if (!userId) {
+      res.status(401).json({
+        success: false,
+        message: "user is not authenticated",
+      });
+      return;
+    }
+
+    const cart = await prisma.cart.upsert({
+      where: { userId },
+      create: { userId },
+      update: {},
+    });
+
+    const cartItem = await prisma.cartItem.upsert({
+      where: {
+        cartId_productId_size_color: {
+          cartId: cart.id,
+          productId: productId,
+          size: size || null,
+          color: color || null,
+        },
+      },
+      update: {
+        quantity: { increment: quantity },
+      },
+      create: {
+        cartId: cart.id,
+        productId,
+        quantity,
+        size: size,
+        color: color || null,
+      },
+    });
+
+    const product = await prisma.product.findUnique({
+      where: { id: productId },
+      select: { name: true, price: true, images: true },
+    });
+
+    const responseItem = {
+      id: cartItem.id,
+      productId: cartItem.productId,
+      name: product?.name,
+      price: product?.price,
+      image: product?.images[0],
+      color: cartItem?.color,
+      size: cartItem?.size,
+      quantity: cartItem.quantity,
+    };
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      error: "failed to add to cart",
+    });
+  }
+};
